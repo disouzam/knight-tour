7daf094870e49096e000e18948486c72cf7b8062
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-06-25 19:36:39 -0300

    Added inheritance to object and other formatting changes

--- python/src/coordinate.py
+++ python/src/coordinate.py
@@ -2,13 +2,19 @@
     A representation of coordinates in a chess board
 """
 
-class Coordinate():
-    def __init__(self, horizontal, vertical) -> None:
+
+class Coordinate(object):
+    """Coordinate():
+    Store information of horizontal and vertical position in a chess board
+    """
+
+    def __init__(self, horizontal: int, vertical: int) -> None:
         self.__horizontal = horizontal
         self.__vertical = vertical
 
     def __str__(self) -> str:
-        result = f"Horizontal: {self.__horizontal}, Vertical: {self.__vertical}"
+        result = f"Horizontal: {
+            self.__horizontal}, Vertical: {self.__vertical}"
         return result
 
     def __repr__(self) -> str:
