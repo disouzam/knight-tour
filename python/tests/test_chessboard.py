b6e18f9c2f313ea6f9bc93214d25721b8ec8a671
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-06-30 18:06:04 -0300

    Added tests to cover exceptional cases in is_occupied function of Chessboard class

--- python/tests/test_chessboard.py
+++ python/tests/test_chessboard.py
@@ -1,8 +1,10 @@
 """
 Tests to ensure correctness of ChessBoard class
 """
+from ctypes import ArgumentError
 from src.coordinate import Coordinate
 from src.chessboard import ChessBoard
+import pytest
 
 
 def test_initialization() -> None:
@@ -15,6 +17,41 @@ def test_initialization() -> None:
         for j in range(0, 8):
             is_occupied = cb.is_occupied(i, j)
             assert not is_occupied
+
+
+def test_checks_of_isoccupied_for_invalid_positions() -> None:
+    """
+    Tries to check if an incorrect position (outside of chessboard) is occupied
+    """
+    cb = ChessBoard()
+
+    # Negative horizontal position
+    with pytest.raises(ArgumentError) as exception_info:
+        _ = cb.is_occupied(-1, 0)
+
+    assert "Horizontal value is incorrect. Must be in range 0 - 7." == str(
+        exception_info.value)
+
+    # Positive horizontal position but out of bounds
+    with pytest.raises(ArgumentError) as exception_info:
+        _ = cb.is_occupied(8, 0)
+
+    assert "Horizontal value is incorrect. Must be in range 0 - 7." == str(
+        exception_info.value)
+
+    # Negative horizontal position
+    with pytest.raises(ArgumentError) as exception_info:
+        _ = cb.is_occupied(0, -1)
+
+    # Positive vertical position but out of bounds
+    assert "Vertical value is incorrect. Must be in range 0 - 7." == str(
+        exception_info.value)
+
+    with pytest.raises(ArgumentError) as exception_info:
+        _ = cb.is_occupied(0, 8)
+
+    assert "Vertical value is incorrect. Must be in range 0 - 7." == str(
+        exception_info.value)
 
 
 def test_string_and_representations_are_the_same() -> None:
